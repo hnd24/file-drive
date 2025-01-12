8dd2d2e882f3cce60458aaf3cff66674b272e97c
diii2409 <duynhut366@gmail.com>
2025-01-13 01:14:29 +0700

    add image config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
 	images: {
 		remotePatterns: [
 			{
+				hostname: "warmhearted-mandrill-113.convex.cloud",
+			},
+			{
 				protocol: "https",
 				hostname: "tangible-dachshund-14.convex.cloud",
 			},
